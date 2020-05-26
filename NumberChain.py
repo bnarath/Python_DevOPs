ff14f772b5a83037dd35eb7610731ac56d8369f2
Bincy Narath <bincybaburajnarath@gmail.com>
2020-05-25 23:39:36 -0400

    Added number chain

--- NumberChain.py
+++ NumberChain.py
@@ -9,18 +9,23 @@ while not done:
         print("Something went wrong")
     else:
         while not done:
-            print(list(range(user_input*loop, user_input*(loop+1))))
+            for no in range(user_input*loop, user_input*(loop+1)):
+                print(no, end='\t')
+            print('\n')
             loop+=1 #First time done
-            try:
-                yes_or_no = input("Want to continue? 'y' or 'n' ").lower()
-                ['y', 'n'].index(yes_or_no)
-            except ValueError:
-                print("Strictly enter 'y' or 'n' ")
-            except:
-                print("Something went wrong")
-            else:
-                if yes_or_no == 'n':
-                    done = True
+            entered_y_or_no = False
+            while not entered_y_or_no:
+                try:
+                    yes_or_no = input("Want to continue? 'y' or 'n' ").lower()
+                    ['y', 'n'].index(yes_or_no)
+                except ValueError:
+                    print("Strictly enter 'y' or 'n' ")
+                except:
+                    print("Something went wrong")
+                else:
+                    entered_y_or_no = True
+                    if yes_or_no == 'n':
+                        done = True
 
 
 
